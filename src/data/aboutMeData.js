b7ed2ad170e31cc80412185031a2e455ed4a9f73
daoptph23912 <thanhdao02dd@gmail.com>
2025-12-09 22:25:53 +0700

    fix bug deploy

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,10 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+		bio: 'I have nearly two years of Frontend experience at Bkav Corporation, where I contributed to feature development and built interfaces for security systems such as EDR, SIEM, and SOAR — many of which involved complex, visually rich dashboards. I also had the opportunity to work alongside senior experts with 10+ years of experience across various fields in IT, web, and desktop development. In the future, I aim to become a Senior Full-stack Developer, continuously learning and contributing to high-quality software products.',
 	},
 	{
 		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+		bio: 'Thank you for visiting my portfolio! If you have any questions or would like to discuss potential collaborations, please feel free to reach out. I am always open to connecting with fellow developers, designers, and professionals in the tech industry. Let\'s create something amazing together!',
 	},
 ];
